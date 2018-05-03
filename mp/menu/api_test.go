564bf1f07c7bc7be0333de6e6f7b0762a6540989
zkfy <lib4net@163.com>
2018-05-03 11:44:21 +0800

    .

---
+++ mp/menu/api_test.go
@@ -0,0 +1,22 @@
+package menu
+
+import (
+	"testing"
+
+	"github.com/micro-plat/wechat/mp"
+)
+
+func TestCreate(t *testing.T) {
+	ctx := mp.NewContext(mp.NewDefaultAccessToken("wx9e02ddcc88e13fd4", "6acb2bf99177524beba3d97d54df2de5"))
+	menu := &Menu{
+		Buttons: []Button{
+			Button{Type: ButtonTypeView, Name: "搜索", URL: "http://www.baidu.com"},
+			Button{Type: ButtonTypeView, Name: "搜索", URL: "http://www.baidu.com"},
+			Button{Type: ButtonTypeView, Name: "搜索", URL: "http://www.baidu.com"},
+		},
+	}
+	err := Create(ctx, menu)
+	if err != nil {
+		t.Error(err)
+	}
+}
