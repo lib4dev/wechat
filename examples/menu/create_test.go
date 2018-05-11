c76ed62fed3ccc844033839c15e5d9fa8f85b46d
zkfy <lib4net@163.com>
2018-05-11 10:05:47 +0800

    增加demo

---
+++ examples/menu/create_test.go
@@ -0,0 +1,27 @@
+package menu
+
+import (
+	"testing"
+
+	"github.com/micro-plat/wechat/mp/menu"
+
+	"github.com/micro-plat/wechat/mp"
+)
+
+func TestCreate(t *testing.T) {
+	tk := mp.NewDefaultAccessTokenByURL("wx9e02ddcc88e13fd4", "6acb2bf99177524beba3d97d54df2de5", "http://192.168.5.71:9999/wx9e02ddcc88e13fd4/wechat/token/get")
+
+	ctx := mp.NewContext(tk)
+	mu := &menu.Menu{
+		Buttons: []menu.Button{
+			menu.Button{Type: menu.ButtonTypeView, Name: "搜索1", URL: "http://www.baidu.com"},
+			menu.Button{Type: menu.ButtonTypeView, Name: "搜索2", URL: "http://www.baidu.com"},
+			menu.Button{Type: menu.ButtonTypeView, Name: "搜索3", URL: "http://www.baidu.com"},
+		},
+	}
+	err := menu.Create(ctx, mu)
+	if err != nil {
+		t.Error(err)
+	}
+
+}
