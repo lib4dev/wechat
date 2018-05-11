1dc120843077ecc8c9fbf9bb3936efe0119d7ba0
zkfy <lib4net@163.com>
2018-05-11 11:45:41 +0800

    修改conf

--- examples/menu/create_test.go
+++ examples/menu/create_test.go
@@ -10,8 +10,7 @@ import (
 
 func TestCreate(t *testing.T) {
 	tk := mp.NewDefaultAccessTokenByURL("wx9e02ddcc88e13fd4", "6acb2bf99177524beba3d97d54df2de5", "http://192.168.5.71:9999/wx9e02ddcc88e13fd4/wechat/token/get")
-
-	ctx := mp.NewContext(tk)
+  ctx := mp.NewContext(tk)
 	mu := &menu.Menu{
 		Buttons: []menu.Button{
 			menu.Button{Type: menu.ButtonTypeView, Name: "搜索1", URL: "http://www.baidu.com"},
